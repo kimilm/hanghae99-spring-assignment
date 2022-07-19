ade361d0652d6491beb0e4f213e6e3a4bfd27488
Kimilm <gnlfla7416@gmail.com>
2022-07-19 22:57:09 +0900

    수정 - 대중교통

--- src/main/java/assignment_01/PublicTransport.java
+++ src/main/java/assignment_01/PublicTransport.java
@@ -4,7 +4,7 @@ public class PublicTransport {
     //차량번호
     private int number;
     //주유량
-    private int fuelVolume;
+    private int fuel;
     //속도
     private int speed;
     //최대 승객 수
@@ -13,17 +13,36 @@ public class PublicTransport {
     //탑승중인 승객 수
     private int passenger;
     //차량상태
-    //private enum status;
     private String status;
+    //수입
+    private int income;
 
     //기본값
     //주유량 = 100
     //속도 = 0
-    public PublicTransport() {
+    protected PublicTransport() {
         //주유량 100
-        fuelVolume = 100;
+        this.fuel = 100;
         //속도 0
-        speed = 0;
+        this.speed = 0;
+        //수입 0
+        this.income = 0;
+    }
+
+    protected PublicTransport(int maxPassengerVolume, String status) {
+        this();
+        this.maxPassengerVolume = maxPassengerVolume;
+        this.status = status;
+    }
+
+    protected PublicTransport(int number, int fuel, int speed, int maxPassengerVolume, int passenger, String status) {
+        this.number = number;
+        this.fuel = fuel;
+        this.speed = speed;
+        this.maxPassengerVolume = maxPassengerVolume;
+        this.passenger = passenger;
+        this.status = status;
+        this.income = 0;
     }
 
     //기능
@@ -33,7 +52,10 @@ public class PublicTransport {
         //speed++;
     }
 
-    //속도 변경
+    public int getSpeed() {
+        return speed;
+    }
+
     public void setSpeed(int speed) {
         int temp = this.speed + speed;
         if (temp < 0) {
@@ -42,7 +64,10 @@ public class PublicTransport {
         this.speed = speed;
     }
 
-    //상태 변경
+    public String getStatus() {
+        return status;
+    }
+
     public void setStatus(String status) {
         this.status = status;
     }
@@ -51,6 +76,7 @@ public class PublicTransport {
     public boolean getOn(int passenger) {
         int on = this.passenger + passenger;
         if (maxPassengerVolume < on) {
+            System.out.println("warning: 최대 승객 수를 초과하였습니다");
             return false;
         }
         this.passenger = on;
@@ -67,10 +93,34 @@ public class PublicTransport {
         return true;
     }
 
-    //**있을 경우** {기타 공통 기능들}
-    //주유하기
+    public int getFuel() {
+        return fuel;
+    }
+
+    public void setFuel(int fuel) {
+        this.fuel = fuel;
+    }
+
     public boolean refuel(int fuel) {
-        this.fuelVolume += fuel;
+        this.fuel += fuel;
+        if (this.fuel < 10) {
+            System.out.println("\nwarning: " + "주유 필요");
+        }
         return true;
+    }
+
+    public int getIncome() {
+        return income;
+    }
+
+    public void setIncome(int income) {
+        this.income = income;
+    }
+
+    public void print() {
+        System.out.println("\ninfo----");
+        System.out.println("탑승 승객 수: " + this.passenger);
+        System.out.println("잔여 승객 수: " + (this.maxPassengerVolume - this.passenger));
+        System.out.println("총 수입: " + this.income);
     }
 }
