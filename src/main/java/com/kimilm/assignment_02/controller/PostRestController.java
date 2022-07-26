4ed50a8b71cfba6dbcc40ae9cd46f68d547baefa
Kimilm <gnlfla7416@gmail.com>
2022-07-26 14:11:33 +0900

    추가 - 게시글 조회 get api

--- src/main/java/com/kimilm/assignment_02/controller/PostRestController.java
+++ src/main/java/com/kimilm/assignment_02/controller/PostRestController.java
@@ -7,10 +7,7 @@ import com.kimilm.assignment_02.repository.PostRepository;
 import com.kimilm.assignment_02.util.PostUtils;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.security.NoSuchAlgorithmException;
 import java.util.HashMap;
@@ -31,8 +28,23 @@ public class PostRestController {
                 .findAllByOrderByCreatedAtDesc().stream()
                 .map(PostListResponseDto::new)
                 .collect(Collectors.toList());
+
         Map<String, Object> result = new HashMap<>();
         result.put(PostUtils.DATA, postListResponseDtoList);
+
+        return ResponseEntity.ok().body(result);
+    }
+
+    // 게시글 조회 api
+    @GetMapping("/api/posts/{id}")
+    public ResponseEntity<Map<String, Object>> getPost(@PathVariable Long id) {
+        Post post = postRepository.findById(id).orElseThrow(
+                () -> new IllegalArgumentException("잘못된 게시글 아이디를 입력하였습니다.")
+        );
+
+        Map<String, Object> result = new HashMap<>();
+        result.put(PostUtils.DATA, post);
+
         return ResponseEntity.ok().body(result);
     }
 
@@ -46,6 +58,6 @@ public class PostRestController {
         result.put(PostUtils.MESSAGE, "작성 완료");
         result.put("id", saved.getId());
 
-    return ResponseEntity.ok().body(result);
+        return ResponseEntity.ok().body(result);
     }
 }
