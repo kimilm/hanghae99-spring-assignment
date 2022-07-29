c7b5e2f87280024b4a4a43a73e38f9da7a4a6b15
Kimilm <gnlfla7416@gmail.com>
2022-07-29 14:20:39 +0900

    수정 - 과제3 모듈 생성

--- assignment-web/src/main/java/com/kimilm/Application.java
+++ assignment-web/src/main/java/com/kimilm/Application.java
@@ -1,4 +1,4 @@
-package com.kimilm.assignment_02;
+package com.kimilm;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -11,3 +11,4 @@ public class Application {
         SpringApplication.run(Application.class, args);
     }
 }
+
