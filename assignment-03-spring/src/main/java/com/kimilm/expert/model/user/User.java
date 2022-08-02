a7c898ddcf1d3eed1a1aa2256839d79705c5f0cb
Kimilm <gnlfla7416@gmail.com>
2022-08-02 21:33:46 +0900

    생성 - UserEntity

--- assignment-03-spring/src/main/java/com/kimilm/expert/model/user/User.java
+++ assignment-03-spring/src/main/java/com/kimilm/expert/model/user/User.java
@@ -1,4 +1,52 @@
 package com.kimilm.expert.model.user;
 
-public class User {
+import com.kimilm.expert.model.Timestamped;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "USERS")
+@NoArgsConstructor
+public class User extends Timestamped {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "USER_ID")
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    @Enumerated(value = EnumType.STRING)
+    private UserRoleEnum role;
+
+    @Column(unique = true)
+    private Long kakaoId;
+
+    public User(String username, String password, String email, UserRoleEnum role) {
+        this.username = username;
+        this.password = password;
+        this.email = email;
+        this.role = role;
+        this.kakaoId = null;
+    }
+
+    public User(String username, String password, String email, UserRoleEnum role, Long kakaoId) {
+        this.username = username;
+        this.password = password;
+        this.email = email;
+        this.role = role;
+        this.kakaoId = kakaoId;
+    }
 }
---
+++ assignment-03-spring/src/main/java/com/kimilm/expert/model/user/UserRoleEnum.java
@@ -0,0 +1,18 @@
+package com.kimilm.expert.model.user;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public enum UserRoleEnum {
+    USER(Authority.USER),
+    ADMIN(Authority.ADMIN);
+
+    private final String authority;
+
+    public static class Authority {
+        public static final String USER = "ROLE_USER";
+        public static final String ADMIN = "ROLE_ADMIN";
+    }
+}
