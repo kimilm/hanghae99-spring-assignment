8b93a94360d71c97dde5ed527c86386fffe70424
Kimilm <gnlfla7416@gmail.com>
2022-08-03 19:06:48 +0900

    수정 - 회원가입시 비밀번호 암호화

--- assignment-03-spring/src/main/java/com/kimilm/expert/service/UserService.java
+++ assignment-03-spring/src/main/java/com/kimilm/expert/service/UserService.java
@@ -4,6 +4,7 @@ import com.kimilm.expert.model.user.User;
 import com.kimilm.expert.model.user.dto.SignupRequestDto;
 import com.kimilm.expert.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -11,12 +12,15 @@ import org.springframework.stereotype.Service;
 public class UserService {
 
     private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
 
     public User register(SignupRequestDto requestDto) {
         // 패스워드 일치하지 않는다면
         if (!requestDto.getPassword().equals(requestDto.getPasswordConfirm())) {
             return null;
         }
+
+        requestDto.setPassword(passwordEncoder.encode(requestDto.getPassword()));
 
         return userRepository.save(new User(requestDto));
     }
