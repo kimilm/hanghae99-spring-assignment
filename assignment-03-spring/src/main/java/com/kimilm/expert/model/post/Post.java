fc601aebe328b0d163a18363995e3d5c9e579083
Kimilm <gnlfla7416@gmail.com>
2022-08-03 21:43:12 +0900

    수정 - GenerationType.IDENTITY 로 수정

--- assignment-03-spring/src/main/java/com/kimilm/expert/model/post/Post.java
+++ assignment-03-spring/src/main/java/com/kimilm/expert/model/post/Post.java
@@ -14,7 +14,7 @@ import java.security.NoSuchAlgorithmException;
 @NoArgsConstructor
 public class Post extends Timestamped {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column(nullable = false)
--- assignment-03-spring/src/main/java/com/kimilm/expert/model/user/User.java
+++ assignment-03-spring/src/main/java/com/kimilm/expert/model/user/User.java
@@ -13,7 +13,7 @@ import javax.persistence.*;
 @NoArgsConstructor
 public class User extends Timestamped {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "USER_ID")
     private Long id;
 
